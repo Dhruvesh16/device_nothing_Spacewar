2cb120adc9d7ed93664deff84abbca70707245b4
Dhruvesh16 <96222424+Dhruvesh16@users.noreply.github.com>
2022-11-30 19:20:56 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
-git clone https://github.com/Dhruvesh16/vendor_nothing_Spacewar.git -b wip vendor/nothing/Spacewar
+git clone https://github.com/Dhruvesh16/vendor_nothing_Spacewar-1.git -b 13 vendor/nothing/Spacewar
 git clone https://github.com/Dhruvesh16/kernel_nothing_sm7325.git kernel/nothing/sm7325
